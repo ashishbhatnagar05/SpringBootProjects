acf58f626456d95992c0e9cedf25972875042372
kaal-PC\kaal <ashishbhatnagar05@gmail.com>
2017-11-13 00:17:52 +0530

    added service layer, made quote get by id

--- SpringBootWeb/src/main/java/com/ashish/springWeb/SpringBootWeb/Controllers/RestController1.java
+++ SpringBootWeb/src/main/java/com/ashish/springWeb/SpringBootWeb/Controllers/RestController1.java
@@ -2,6 +2,8 @@ package com.ashish.springWeb.SpringBootWeb.Controllers;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.ashish.springWeb.SpringBootWeb.Controllers.Model.Quote;
@@ -21,15 +23,19 @@ public class RestController1 {
 		this.quoteListService = quoteListService;
 	}
 
-	@RequestMapping(value = "/welcome")
+	@GetMapping("/welcome")
 	public String displayWelcomeMessage() {
 		return "Hi, Welcome to Ashish's First Rest Web application";
 	}
 
-	@RequestMapping("/quotes")
+	@GetMapping("/quotes")
 	public List<Quote> getQuotes() {
 		return quoteListService.getQuotes();
 
 	}
 
+	@GetMapping("/quote/{id}")
+	public Quote getQuoteById(@PathVariable Integer id) {
+		return quoteListService.getQuoteById(id);
+	}
 }
--- SpringBootWeb/src/main/java/com/ashish/springWeb/SpringBootWeb/Service/QuoteListService.java
+++ SpringBootWeb/src/main/java/com/ashish/springWeb/SpringBootWeb/Service/QuoteListService.java
@@ -3,14 +3,26 @@ package com.ashish.springWeb.SpringBootWeb.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import com.ashish.springWeb.SpringBootWeb.Controllers.Model.Quote;
 
 @Service
+@Scope("singleton")
 public class QuoteListService {
+	List<Quote> listOfQuotes;
+
 	public List<Quote> getQuotes() {
-		List<Quote> listOfQuotes = new ArrayList<Quote>();
+		return listOfQuotes;
+	}
+
+	public Quote getQuoteById(Integer id) {
+		return listOfQuotes.get(id - 1);
+	}
+
+	public QuoteListService() {
+		listOfQuotes = new ArrayList<Quote>();
 		Quote quote1 = new Quote();
 		quote1.setId(1);
 		quote1.setQuote("Love yourself");
@@ -23,6 +35,5 @@ public class QuoteListService {
 		listOfQuotes.add(quote1);
 		listOfQuotes.add(quote2);
 		listOfQuotes.add(quote3);
-		return listOfQuotes;
 	}
 }
